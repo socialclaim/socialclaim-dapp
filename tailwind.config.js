f3c03a0785a547c748a5f5081280257e65daa8a0
Yann Guillon <yann@dockdev.com>
2022-01-19 14:43:17 +0000

    last

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundImage: (theme) => ({
+      backgroundImage: () => ({
         dunes: "url('/images/bg.jpg')",
       }),
     }
